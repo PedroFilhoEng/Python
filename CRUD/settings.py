6adf1dce5d369d219e112e6687da3813ba886733
PedroFilhoEng <35050296+PedroFilhoEng@users.noreply.github.com>
2023-04-17 03:44:46 -0300

    X

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -135,6 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+#del DATABASES['default']['OPTIONS']['sslmode']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
