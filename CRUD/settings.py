6d2e66720d9e22941e6cbfa895bb6ce75961def4
PedroFilhoEng <35050296+PedroFilhoEng@users.noreply.github.com>
2023-04-16 23:08:20 -0300

    Primeiro Upload DB MYSQL ssl

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -95,8 +95,7 @@ DATABASES = {
         'HOST': 'us-cdbr-east-06.cleardb.net',
         'USER': 'b730adbf400653',
         'PASSWORD': 'fe529226',
-        'OPTIONS': {
-            'ssl_disabled': True,},
+        'OPTIONS': {'ssl': {'ca':'/cleardb-ca.pem', 'cert':'/b730adbf400653-cert.pem', 'key':'/ b730adbf400653-key.pem'},},
     }
 }
 
