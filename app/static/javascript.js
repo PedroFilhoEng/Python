f9d4673eccf971dd07b55c9194ced68ea6660ff2
PedroFilhoEng <35050296+PedroFilhoEng@users.noreply.github.com>
2023-04-21 22:29:52 -0300

    correção form

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -14,36 +14,45 @@
         }
     }
 
-//Ajax do form
-    if(doc.querySelector('#form')){
-        let form =doc.querySelector('#form')
-        function sendForm(event)
-        {
-            event.preventDefault();
-            let data = new FormData(form);
-            let ajax = new XMLHttpRequest();
-            let token =  doc.querySelectorAll('input');[0].value;
-            ajax.open('POST', form.action);
-            ajax.setRequestHeader('X-CSRF-TOKEN', token);
-            ajax.onreadystatechange = function ()
-            {
-                if(ajax.status === 200 && ajax.readyState === 4){
-                    let result = doc.querySelector('#result');
-                    result.innerHTML = 'Cadastro realizado com sucesso';
-                    result.classList.add('alert');
-                    result.classList.add('alert-success');
-                }
-            }
-            ajax.send(data);
-            form.reset();
+    //Ajax do form
+    if (doc.querySelector('#form')) {
+  let form = doc.querySelector('#form')
+  function sendForm(event) {
+    event.preventDefault();
+    let data = new FormData(form);
+    let ajax = new XMLHttpRequest();
+    let token = doc.querySelectorAll('input')[0].value;
+    ajax.open('POST', form.action);
+    ajax.setRequestHeader('X-CSRF-TOKEN', token);
+    ajax.onreadystatechange = function () {
+      if (ajax.readyState === 4) {
+        if (ajax.status === 200) {
+          let response = JSON.parse(ajax.responseText);
+          if (response.status === 'success') {
+            let result = doc.querySelector('#result');
+            result.innerHTML = 'Cadastro realizado com sucesso';
+            result.classList.add('alert');
+            result.classList.add('alert-success');
+          } else if (response.status === 'error') {
+            let result = doc.querySelector('#result');
+            result.innerHTML = response.message;
+            result.classList.add('alert');
+            result.classList.add('alert-danger');
+          }
+        } else {
+          let result = doc.querySelector('#result');
+          result.innerHTML = 'Ocorreu um erro ao enviar o formulário. Por favor, tente novamente mais tarde.';
+          result.classList.add('alert');
+          result.classList.add('alert-danger');
         }
-        form.addEventListener('submit', sendForm,false)
-    }
+      }
+    };
+    ajax.send(data);
+    form.reset();
+  }
+  form.addEventListener('submit', sendForm, false);
+}
 
-    // In your Javascript (external .js resource or <script> tag)
-    $(document).ready(function() {
-        $('.js-example-basic-multiple').select2();
-    });
 
 
 })(window,document);
